6735718d0cbdccb6e57df2647b475e7ddd7baa24
gustavocsverdrup <gustavo.catalasverdrup@gmail.com>
2020-12-27 02:28:22 +0100

    making country search to functional component and using material UI table

--- src/CountrySearch.js
+++ src/CountrySearch.js
@@ -1,44 +1,77 @@
 import axios from "axios";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
 import Moment from "react-moment";
 import Key from "./keys";
 import "./App.css";
 
 import Travelrec from "./Travelrec";
 
-class CountrySearch extends React.Component {
-  state = {
-    data: [],
-    country: "",
-    showsearch: "",
-    errormsg: false,
-    rawData: [],
-    showRecs: false,
-    travelData: [],
-  };
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
 
-  componentDidMount = () => {
-    this.setState({ showsearch: true });
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 700,
+  },
+});
+
+const CountrySearch = () => {
+  const [data, setData] = useState([]);
+  const [country, setCountry] = useState("");
+  const [showsearch, setSearch] = useState("");
+  const [errormsg, setError] = useState(false);
+  const [rawData, setRawData] = useState([]);
+  const [showRecs, setRecs] = useState(false);
+  const [travelData, setTravelData] = useState([]);
+  const classes = useStyles();
+  const [countryData, setCountryData] = useState("");
+
+  useEffect(() => {
+    setSearch(true);
 
     axios
       .get("https://www.trackcorona.live/api/travel")
-      .then((res) => this.setState({ rawData: res.data.data }))
+      .then((res) => setData(res.data.data))
       .catch((err) => console.log(err));
-  };
+  }, []);
 
-  getFromApi = (e) => {
+  const getFromApi = (e) => {
     e.preventDefault();
 
-    this.setState({ showsearch: false });
+    setSearch(false);
 
-    if (this.state.country == "") {
-      this.setState({ errormsg: true });
+    if (country == "") {
+      setError(true);
 
       return;
     }
 
     fetch(
-      `https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats?country=${this.state.country}`,
+      `https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats?country=${country}`,
       {
         method: "GET",
         headers: {
@@ -48,125 +81,130 @@ class CountrySearch extends React.Component {
       }
     )
       .then((response) => response.json())
-      .then((data) => this.outputting(data));
+      .then((data) => outputting(data));
   };
 
-  changeCountry = (e) => {
-    this.setState({ country: e.target.value, data: [] });
+  const changeCountry = (e) => {
+    setCountry(e.target.value);
+    setData([]);
   };
 
-  outputting = (dat) => {
-    if (!dat.data.covid19Stats.length < 218 && this.state.country) {
-      let dataarr = [...this.state.data];
+  const outputting = (dat) => {
+    if (!dat.data.covid19Stats.length < 218 && country) {
+      let dataarr = [...data];
 
       if (!dat.data.covid19Stats || dat.data.covid19Stats[0].country === "US") {
-        this.setState({ errormsg: true });
+        setError(true);
         return;
       }
 
       dataarr = dat.data.covid19Stats.map((region) => {
         return {
           region: region,
-          province: <p>{region.province}</p>,
-          confirmed: <p>{region.confirmed}</p>,
-          recovered: <p>{region.recovered}</p>,
-          deaths: <p>{region.deaths}</p>,
-          timestamp: <p>{region.lastUpdate}</p>,
+          province: region.province,
+          confirmed: region.confirmed,
+          recovered: region.recovered,
+          deaths: region.deaths,
+          timestamp: region.lastUpdate,
         };
       });
 
       let travelInfoArr;
       travelInfoArr = [];
 
-      this.state.rawData.map((country) => {
+      rawData.map((country) => {
         if (country.location === dat.data.covid19Stats[0].country) {
           travelInfoArr.push(country.data);
         }
-        this.setState({ travelData: travelInfoArr });
+        setTravelData(travelInfoArr);
       });
 
-      this.setState({ data: dataarr });
+      setData(dataarr);
     }
   };
 
-  newSearch = () => {
-    this.setState({ country: "", showsearch: true, errormsg: false });
+  const newSearch = () => {
+    setCountry("");
+    setSearch(true);
+    setError(false);
   };
-  showRecommendations = () => {
-    this.setState({ showRecs: !this.state.showRecs });
+  const showRecommendations = () => {
+    setRecs(!showRecs);
   };
-  render() {
-    const countrydata = this.state.data.map((cas, index) => (
-      <tbody key={index}>
-        <tr className="card">
-          <td>{cas.province}</td>
-          <td>{cas.confirmed}</td>
-          <td>{cas.deaths}</td>
-          <td>{cas.recovered}</td>
-          <td>
-            <Moment durationFromNow>{cas.timestamp}</Moment> from now
-          </td>
-        </tr>
-      </tbody>
-    ));
 
-    return this.state.showsearch == true ? (
+  useEffect(() => {
+    setCountryData(
+      data.map((cas, index) => (
+        <TableBody key={index}>
+          <StyledTableRow key={index}>
+            <StyledTableCell align="center">{cas.province}</StyledTableCell>
+            <StyledTableCell align="center">{cas.confirmed}</StyledTableCell>
+            <StyledTableCell align="center">{cas.deaths}</StyledTableCell>
+            <StyledTableCell align="center">{cas.recovered}</StyledTableCell>
+            <StyledTableCell align="center">
+              <Moment durationFromNow>{cas.timestamp}</Moment> from now
+            </StyledTableCell>
+          </StyledTableRow>
+        </TableBody>
+      ))
+    );
+  }, [data]);
+
+  return showsearch == true ? (
+    <div>
+      <h3>ENTER COUNTRY NAME (Excepting China and the US)</h3>
+
+      <form className="forma" onSubmit={getFromApi}>
+        <input
+          id="countryruta"
+          type="text"
+          onChange={(e) => changeCountry(e)}
+        />
+        <button id="countrysearchbtn" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  ) : showsearch == false && errormsg == false ? (
+    <div className="countryinfo">
+      <h3 className="country_title">{country.toUpperCase()}</h3>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="center">PROVINCE</StyledTableCell>
+              <StyledTableCell align="center">CONFIRMED</StyledTableCell>
+              <StyledTableCell align="center">CASUALTIES</StyledTableCell>
+              <StyledTableCell align="center">RECOVERED</StyledTableCell>
+              <StyledTableCell align="center">
+                UPDATED (hh:mm:ss)
+              </StyledTableCell>
+            </TableRow>
+          </TableHead>
+          {countryData}
+        </Table>
+      </TableContainer>
+      <button className="backToTop" onClick={newSearch}>
+        New Search
+      </button>
+      <p id="recommendations_link" onClick={() => showRecommendations()}>
+        TRAVEL RECOMMENDATIONS
+      </p>
+    </div>
+  ) : errormsg == true && showsearch == false ? (
+    <div className="notvalidcountry">
+      You need to enter a valid country name
       <div>
-        <h3>ENTER COUNTRY NAME (Excepting China and the US)</h3>
-
-        <form className="forma" onSubmit={this.getFromApi}>
-          <input
-            id="countryruta"
-            type="text"
-            onChange={(e) => this.changeCountry(e)}
-          />
-          <button id="countrysearchbtn" type="submit">
-            Search
-          </button>
-        </form>
-      </div>
-    ) : this.state.showsearch == false && this.state.errormsg == false ? (
-      <div className="countryinfo">
-        <h3 className="country_title">
-          COUNTRY DATA for {this.state.country.toUpperCase()}
-        </h3>
-        <table>
-          <thead>
-            <tr>
-              <th>PROVINCE</th>
-              <th>CONFIRMED</th>
-              <th>CASUALTIES</th>
-              <th>RECOVERED</th>
-              <th>UPDATED (hh:mm:ss)</th>
-            </tr>
-          </thead>
-          {countrydata}
-        </table>
-        <button className="backToTop" onClick={this.newSearch}>
+        <button className="backToTop" onClick={newSearch}>
           New Search
         </button>
-        <p id="recommendations_link" onClick={() => this.showRecommendations()}>
-          TRAVEL RECOMMENDATIONS
-        </p>
       </div>
-    ) : this.state.errormsg == true && this.state.showsearch == false ? (
-      <div className="notvalidcountry">
-        You need to enter a valid country name
-        <div>
-          <button className="backToTop" onClick={this.newSearch}>
-            New Search
-          </button>
-        </div>
-      </div>
-    ) : (
-      this.state.showRecs && (
-        <Travelrec
-          travelData={this.state.travelData}
-          closeRecs={this.showRecommendations}
-        />
-      )
-    );
-  }
-}
+    </div>
+  ) : (
+    showRecs && (
+      <Travelrec travelData={travelData} closeRecs={showRecommendations} />
+    )
+  );
+};
 
 export default CountrySearch;
