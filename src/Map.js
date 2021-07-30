96e6b251530348d049b76e7f539c517f09709840
gustavocsverdrup <gustavo.catalasverdrup@gmail.com>
2021-07-30 19:20:49 +0200

    setting the tooltip overlay on the whole map and the map as a child but not seeing the actual overlay component around the svg of the map - weird

--- src/Map.js
+++ src/Map.js
@@ -4,15 +4,17 @@ import Tooltip from "react-bootstrap/Tooltip";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 // import Button from "react-bootstrap/Button";
 import WorldMap from "react-world-map";
+// import shadows from "@material-ui/core/styles/shadows";
 
 function Map() {
-  const [selected, onSelect] = useState(null);
+  // const [selected, onSelect] = useState(null);
   const [show, setShow] = useState(false);
+  // const [continent, setContinent] = useState("");
   const target = useRef(null);
 
   const showStats = (cont) => {
-    document.getElementById(`${cont.toUpperCase()}`).style.border =
-      "2px solid black";
+    // onSelect(cont);
+    console.log(cont);
   };
 
   return (
@@ -21,23 +23,16 @@ function Map() {
       <OverlayTrigger
         target={target.current}
         show={show}
+        children={<WorldMap />}
         delay={{ hide: 450, show: 300 }}
+        onClick={(e) => showStats(e)}
         overlay={(props) => (
           <Tooltip {...props}>
             Hii, I am a simple tooltip information!!!
           </Tooltip>
         )}
         placement="bottom"
-      >
-        <WorldMap
-          ref={target}
-          selected={selected}
-          onSelect={(continent) => {
-            showStats(continent);
-          }}
-          onClick={() => setShow(!show)}
-        />
-      </OverlayTrigger>
+      ></OverlayTrigger>
     </>
   );
 }
