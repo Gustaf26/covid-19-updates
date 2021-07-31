98e3e0d8044103c189d99d1b6bcc87ca3c751c68
gustavocsverdrup <gustavo.catalasverdrup@gmail.com>
2021-07-31 15:48:04 +0200

    showing a simple tooltip with continent names onSelect and keeping the yellow color on continent selected

--- src/Map.js
+++ src/Map.js
@@ -1,36 +1,36 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import Tooltip from "react-bootstrap/Tooltip";
-import Overlay from "react-bootstrap/Overlay";
+// import Tooltip from "react-bootstrap/Tooltip";
+// import Overlay from "react-bootstrap/Overlay";
 // import Button from "react-bootstrap/Button";
 import WorldMap from "react-world-map";
-// import shadows from "@material-ui/core/styles/shadows";
 
 function Map() {
-  // const [selected, onSelect] = useState(null);
-  const [show, setShow] = useState(false);
-  const target = useRef(null);
-  // const [continent, setContinent] = useState("");
+  const [selected, onSelect] = useState(null);
+  const selectionIndex = useRef(0);
 
-  const showStats = (cont) => {
-    // onSelect(cont);
-    console.log(cont);
+  const getMyToolTipFunction = (cont) => {
+    onSelect(cont);
+    selectionIndex.current += 1;
+    const popup = document.getElementById("displayText");
+    if (selectionIndex.current === 1) {
+      popup.toggleAttribute("hidden");
+    }
+    popup.innerText = cont;
   };
 
   return (
     <>
       <h5 className="pb-2">See stats on each continent</h5>
-      <WorldMap ref={target} onClick={(e) => showStats(e)} />
-      <Overlay
-        delay={{ hide: 450, show: 300 }}
-        target={target.current}
-        show={show}
-        placement="right"
-      >
-        {({ placement, arrowProps, show: _show, popper, ...props }) => (
-          <Tooltip {...props}>Simple tooltip</Tooltip>
-        )}
-      </Overlay>
+      <div class="class">
+        <WorldMap
+          selected={selected}
+          onSelect={(cont) => getMyToolTipFunction(cont)}
+        />
+        <span hidden class="displayText" id="displayText">
+          Hi I am ToolTip
+        </span>
+      </div>
     </>
   );
 }
